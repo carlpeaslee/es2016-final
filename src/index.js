ed83233235babb0dd75d26e24fb85ec285c492ad
Carl Peaslee <cpeaslee@gmail.com>
2017-06-18 01:43:19 -0500

    ready to start

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import {
+  // constAndLet,
+  // importing,
+  // arrowFunctions,
+  // destructuring,
+  // objAssemblyComputedProps,
+  // spreadOperator,
+  // templateLiterals,
+  // functionArgs
+} from './examples'
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
